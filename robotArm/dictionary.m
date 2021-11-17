6e93437650a444a767e977ff9ef7f581ff2174d7
Lu Shi <31721160+sunnyshi0310@users.noreply.github.com>
2021-11-17 13:58:06 -0800

    Update dictionary.m

--- robotArm/dictionary.m
+++ robotArm/dictionary.m
@@ -1,30 +1,8 @@
 function x_phi = dictionary(x, u, index_j, index_d)%
 % x = theta1, theta2, theta1_dot, theta2_dot, u= tau1 tau2
 % check the number of links    
-if index_j == 2
-    if index_d == 1
-        x1_2r = radial(x(1),2);
-        x2_2r = radial(x(2),2);
-        u1 = [1,u(1)];
-        u2 = [1,u(2)];
-        x_ext = kron(x1_2r,x2_2r);
-        u_ext = kron(u1,u2);
-        x_phi = kron(x_ext,u_ext);
-    end
-    
-    if index_d == 2
-        x1_2r = radial(x(1),2);
-        x2_2r = radial(x(2),2);
-        x3 = [1,x(3)];
-        x4 = [1,x(4)];
-        u1 = [1,u(1)];
-        u2 = [1,u(2)];
-        x_ext = kron(kron(kron(x1_2r,x2_2r),x3),x4);
-        u_ext = kron(u1,u2);
-        x_phi = kron(x_ext, u_ext);        
-    end
-    
-    if index_d == 3
+if index_j == 2    
+    if index_d == 1 % ACD-EDMD
         x1_2r = [1, x(1)];
         x2_2r = [1, x(2)];
         x3 = [1,x(3)];
@@ -36,21 +14,8 @@ if index_j == 2
         x_phi = kron(x_ext, u_ext);  %[x_ext,u(1),u(2)];%      
     end    
     
-    if index_d == 4
-        x1_2r = [1, x(1), x(1)^2-1];
-        x2_2r = [1, x(2), x(2)^2-1];
-        u1 = [1,u(1)];
-        u2 = [1,u(2)];
-        x_ext = kron(x1_2r,x2_2r);
-        u_ext = kron(u1,u2);
-        x_phi = kron(x_ext, u_ext); 
-    end
-    
-    if index_d == 5
-        
-        x_phi = [x(1),x(2),u(1),u(2)]; 
-    end
-    if index_d == 6
+
+    if index_d == 6 % Hermite polynomial
         x1_2h = [1, x(1), 1*x(1)^2-1];
         x2_2h = [1, x(2), 1*x(2)^2-1];
         x3_3h = [1, x(3), 1*x(3)^2-1];
@@ -62,22 +27,4 @@ if index_j == 2
         x_phi = kron(u_ext,x_ext);
     end
     
-    if index_d == 7
-        x1_2h = [1, x(1), 1*x(1)^2-1];
-        x2_2h = [1, x(2), 1*x(2)^2-1];
-        u1 = [1,u(1)];
-        u2 = [1,u(2)];
-        x_ext = kron(x1_2h,x2_2h);
-        u_ext = kron(u1, u2);
-        x_phi = kron(u_ext,x_ext);
-    end
-
-    if index_d == 9
-        x1_2r = [1, x(1)];
-        x2_2r = [1, x(2)];
-        x3 = [1,x(3)];
-        x4 = [1,x(4)];
-        x_ext = kron(kron(kron(x1_2r,x2_2r),x3),x4);
-        x_phi = x_ext;     
-    end
 end

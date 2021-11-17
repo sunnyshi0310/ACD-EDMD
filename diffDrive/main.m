74407d3ad70119739af06d5f27e8dbdcd44a8e46
Lu Shi <31721160+sunnyshi0310@users.noreply.github.com>
2021-11-17 13:23:40 -0800

    Update main.m

--- diffDrive/main.m
+++ diffDrive/main.m
@@ -14,13 +14,8 @@ tic
 %% Estimation the Koopman operator  
 load('INPUTS.mat'); % load the input data
 K = initialization(index_d, Ts, nx, nu, INPUTS); % ACD-EDMD
-% K = init_comp(index_d, Ts, nx, nu, INPUTS); % Hermite polynomial and SINDy
 % [K, W_out,b_out,W0,W1,W2,b0,b1,b2,e] = init_EDMDDL(Ts, nx, nu, INPUTS);
 
 t0 = toc % print the training time
 F = doubleV(K,nx,nu,index_d);% calculate the F matrix from K
 
-%% check the performance of prediction
-performance(F,Ts,nx,nu,index_d);
-% check_sindy(K,Ts,nx,nu);
-% check_DL(F,Ts,nx, W_out,b_out,W0,W1,W2,b0,b1,b2)

ba89c8076c5308e26a3214bf7872f896c4755f1c
Lu Shi <31721160+sunnyshi0310@users.noreply.github.com>
2021-11-19 16:52:56 -0800

    noise: optinal

--- diffDrive/initialization.m
+++ diffDrive/initialization.m
@@ -8,7 +8,7 @@ M = 100; % number of tests
 l_k = 500; % length for estimation
 % initialize the estimated Koopman operater
 K = zeros(length(dictionary(zeros(nx+1,1),zeros(nu,1),index_d)));
-noise = zeros(nx,1); % optional
+% noise: optional
 
 for i = 1:M
     x_test(:,1) = zeros(nx,1);    
@@ -19,7 +19,7 @@ for i = 1:M
     % propagate the system
     for j = 1:l_k
         x_test(:,j+1) = diffDriveDT(x_test(:,j),u_hat(:,j),Ts);
-        x_hat(1:2,j+1) = x_test(1:2,j+1)+noise;
+        x_hat(1:2,j+1) = x_test(1:2,j+1); % +noise;
         x_hat(3:4,j+1) = [sin(x_test(3,j+1)),cos(x_test(3,j+1))];
         
         u_hat(1,j+1) = INPUTS(1,i,j+1); 
